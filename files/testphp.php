10b6fe8eb066be480e0b2afe993cb4dbf2a1e505
Chris Andrisevic <candrisevic@gmail.com>
2012-09-08 11:47:46 -0400

    initial commit for testphp

---
+++ files/testphp.php
@@ -0,0 +1,9 @@
+<?php
+
+$movies[0]="Friday the 13th";
+$movies[1]="Evil Dead";
+$movies[2]="Office Space";
+$movies[3]="Spaceballs";
+echo $movies[0] . " and " . $movies[1] . " are Horror Films.";
+
+?> 
