43fa4c9d007194f480637265ac4da1f11d704fd5
Chris Andrisevic <candrisevic@gmail.com>
2012-09-10 12:58:55 -0400

    added more arrays

--- files/testphp.php
+++ files/testphp.php
@@ -1,9 +1,22 @@
 <?php
 
+define('LNBREAK', '<br /><br />');
+
 $movies[0]="Friday the 13th";
 $movies[1]="Evil Dead";
 $movies[2]="Office Space";
 $movies[3]="Spaceballs";
+$movies[4]="Bladerunner";
+$movies[5]="Gattaca";
+
 echo $movies[0] . " and " . $movies[1] . " are Horror Films.";
+
+print LNBREAK;
+
 echo $movies[2] . " and " . $movies[3] . " are Comedy Films.";
+
+print LNBREAK;
+
+echo $movies[4] . " and " . $movies[5] . " are Sci-Fi Films.";
+
 ?> 
